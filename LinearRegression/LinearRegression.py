472809c8cf2d12fca0b2905f6825b0e20c966197
HOANG Nguyen Minh <minhhoangsss99@gmail.com>
2020-12-15 08:33:29 +0700

    add linear regression

---
+++ LinearRegression/LinearRegression.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('data_linear.csv').values
+print(data)
+print(type(data))  # <class 'numpy.ndarray'>
+print(data.shape)  # (30, 2)
+
+N = data.shape[0]  # N = number of rows = 30
+# data.shape[0] = number of rows
+# data.shape[1] = number of columns
+x = data[:, 0].reshape(-1, 1)
+print(x)
+print(type(x))  # <class 'numpy.ndarray'>
+print(x.shape)  # (30, 1)
+y = data[:, 1].reshape(-1, 1)
+
+plt.scatter(x, y)
+plt.xlabel("Mét vuông")
+plt.ylabel('Giá')
+
+x = np.hstack((np.ones((N, 1)), x))
+w = np.array([0., 1.]).reshape(-1, 1)
+
+numOfIteration = 100
+cost = np.zeros((numOfIteration, 1))
+learning_rate = 0.000001
+
+for i in range(1, numOfIteration):
+    r = np.dot(x, w) - y
+    cost[i] = 0.1 * np.sum(r * r)
+    w[0] -= learning_rate * np.sum(r)
+    w[1] -= learning_rate * np.sum(np.multiply(r, x[:, 1].reshape(-1, 1)))
+    print(cost[i])
+predict = np.dot(x, w)
+plt.plot((x[0][1], x[N - 1][1]), (predict[0], predict[N - 1]), 'r')
+plt.show()
+
+x1 = 50
+y1 = w[0] + w[1] * 50
+print('Giá nhà cho 50m^2 là:', y1)
